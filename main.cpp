fdd8455a2ec84850d49875159ee564aebbe4970a
Kyu Lee <kwooler@gmail.com>
2026-03-11 01:21:00 -0700

    Solution code

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main()
 {
 	int prev, current;
-	/* ****************************
-	// Complete this code.
-	*************************** */
+	// TODO
+	int flag = 1;
+
+	cin >> current;
+	while ((flag == 1) or (prev > current))
+	{
+		flag = 0;
+		cout << current << "\t";
+		prev = current;
+		cin >> current;
+	}
+	// TODO
 }
